8903c90f2d75aeb4bb50176e8c7eebfb76a0f87d
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-01-01 13:45:50 +0000

    [Add]ブラックジャックにエースを引いた時のルール実装

--- training6.rb
+++ training6.rb
@@ -1,10 +1,13 @@
 # 疑似ブラックジャック
-def hit_check(card)
-    total = card + (rand(10) + 1)
-    puts("あなたのカードの合計は#{total}です")
+def hit_check(card,name)
+    if card < 11
+        total = card + (rand(11) + 1)
+    else
+        total = card + (rand(10) + 1)
+    end
+    puts("#{name}のカードの合計は#{total}です")
     return total
 end
-
 
 def bust_check(card)
     if card > 21
@@ -27,33 +30,42 @@ def battle(p_card,d_card)
     end
 end
 
+user = "あなた"
+cpu = "ディーラー"
 
+d_card = 0
+d_card = hit_check(d_card,cpu)
+p_card = (rand(11) + 1)
+p_card = hit_check(p_card,user)
 
-
-d_card = rand(10) + 1
-puts("ディーラーのカードは#{d_card}です")
-p_card = (rand(10) + 1) + (rand(10) + 1)
-puts("あなたのカードの合計は#{d_card}です")
-
-puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
-key = gets.to_i
+unless p_card == 21
+    puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
+    key = gets.to_i
+else
+    key = 2
+end
 
 while key == 1
-    p_card = hit_check(p_card)
+    p_card = hit_check(p_card,user)
     if bust_check(p_card)
         puts("あなたの負けです")
         exit
     else
-        puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
-        key = gets.to_i
+        unless p_card == 21
+            puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
+            key = gets.to_i
+        else
+            key = 2
+        end
     end
 end
 
 if key == 2
-    while d_card < 16
+    puts ("ディーラーが引きます")
+    d_card = hit_check(d_card,cpu)
+    while d_card < 16 do
         puts ("ディーラーが引きます")
-        d_card = d_card + (rand(10) + 1)
-        puts("ディーラーのカードの合計は#{d_card}です")
+        d_card = hit_check(d_card,cpu)
     end
     if bust_check(d_card)
         puts("あなたの勝ちです")
