6ae153a1fcb12528a7ae457787f2667c6965f9fc
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-01-02 13:41:52 +0000

    [Add]ブラックジャックにコンティニュー機能追加

--- training6.rb
+++ training6.rb
@@ -20,59 +20,73 @@ end
 def battle(p_card,d_card)
     if p_card > d_card
         puts("あなたの勝ちです")
-        exit
     elsif p_card == d_card
         puts("引き分けです")
-        exit
     else
         puts("あなたの負けです")
+    end
+end
+
+def continue_check
+    puts("ゲームを続けますか？")
+    puts("ゲームを続ける＝1,やめる＝それ以外のキー")
+    input = gets.to_i
+    if input == 1
+        return 1
+    else
         exit
     end
 end
+
 
 user = "あなた"
 cpu = "ディーラー"
+continue = 1
 
-d_card = 0
-d_card = hit_check(d_card,cpu)
-p_card = (rand(11) + 1)
-p_card = hit_check(p_card,user)
-
-unless p_card == 21
-    puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
-    key = gets.to_i
-else
-    key = 2
-end
-
-while key == 1
+while continue == 1 do
+    d_card = 0
+    d_card = hit_check(d_card,cpu)
+    p_card = (rand(11) + 1)
     p_card = hit_check(p_card,user)
-    if bust_check(p_card)
-        puts("あなたの負けです")
-        exit
+    
+    unless p_card == 21
+        puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
+        key = gets.to_i
     else
-        unless p_card == 21
-            puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
-            key = gets.to_i
+        key = 2
+    end
+    
+    while key == 1
+        p_card = hit_check(p_card,user)
+        if bust_check(p_card)
+            puts("あなたの負けです")
+            continue = continue_check
+            key = 0
         else
-            key = 2
+            unless p_card == 21
+                puts("カードを引く＝1,チェックする=2,やめる＝それ以外のキー")
+                key = gets.to_i
+            else
+                key = 2
+            end
+        end
+    end
+    
+    if key == 2
+        puts ("ディーラーが引きます")
+        d_card = hit_check(d_card,cpu)
+        while d_card < 16 do
+            puts ("ディーラーが引きます")
+            d_card = hit_check(d_card,cpu)
+        end
+        if bust_check(d_card)
+            puts("あなたの勝ちです")
+            continue = continue_check
+            key = 0
+        else
+            battle(p_card,d_card)
+            continue = continue_check
+            key = 0
         end
     end
 end
-
-if key == 2
-    puts ("ディーラーが引きます")
-    d_card = hit_check(d_card,cpu)
-    while d_card < 16 do
-        puts ("ディーラーが引きます")
-        d_card = hit_check(d_card,cpu)
-    end
-    if bust_check(d_card)
-        puts("あなたの勝ちです")
-    else
-        battle(p_card,d_card)
-    end
-else
-    exit
-end
-
